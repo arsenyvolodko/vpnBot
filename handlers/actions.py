15aa09940c52b02caedf0df0eb74628eda1b4c17
arsenyvolodko <arvolodko@gmail.com>
2023-11-27 15:30:16 +0300

    fixed 300

--- handlers/actions.py
+++ handlers/actions.py
@@ -382,7 +382,7 @@ async def callback_inline(call: types.CallbackQuery):
     elif call.data in FILL_UP_BALANCE_CALLBACKS_MAP:
         sum_value = FILL_UP_BALANCE_CALLBACKS_MAP[call.data]
         balance = botDB.get_balance(call.from_user.id)
-        if balance + sum_value >= 300:
+        if balance + sum_value > 300:
             await call.bot.edit_message_text(chat_id=call.from_user.id, message_id=call.message.message_id,
                                              text=f"Поскольку бот работает в тестовом режиме, сумма средств на счете временно ограничена 300₽.\n"
                                                   f"На вашем счете: {balance}₽.",
