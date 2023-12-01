2aec77002f630ad60eca0db3d2752e969ddae2b9
arsenyvolodko <arvolodko@gmail.com>
2023-12-02 02:02:36 +0300

    fixed bugs

--- send_msg_to_all_users.py
+++ send_msg_to_all_users.py
@@ -1,20 +1,28 @@
 from files import Files
 from shared import bot
 from shared import botDB
+import asyncio
 from constants import *
 
 
-# users = botDB.get_all_users()
-
-def send_msg_safety(user_id, text):
+async def send_msg_safety(user_id, text):
     try:
         bot.bot.send_message(chat_id=user_id, text=text)
     except Exception as e:
         Files.write_to_logs(f"User {user_id} blocked bot")
 
 
-users = [MY_ID]
-for user in users:
-    send_msg_safety(user, 'Короче я потестил, как работает списания и продление подписки, все ок, '
-                          'так что теперь номинальная стоимость подписки - 100 рублей в месяц и списания соответственно тоже раз в месяц. '
-                          'Дату следующего списания лапками всем перенес ровно на месяц.')
+async def send_msg():
+    users = [MY_ID]
+    # users = botDB.get_all_users()
+    for user in users:
+        await send_msg_safety(user, 'Короче я потестил, как работает списания и продление подписки, все ок, '
+                                    'так что теперь номинальная стоимость подписки - 100 рублей в месяц и списания соответственно тоже раз в месяц. '
+                                    'Дату следующего списания лапками всем перенес ровно на месяц.')
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(send_msg())
+    botDB.close()
+    loop.close()
