472ff857055c08a38851b2a79be1ef867670a569
Volodko Arseny <Volodko.Arseny@infotecs-nt>
2024-04-12 10:00:48 +0300

    |

--- handlers/payments.py
+++ handlers/payments.py
@@ -1,38 +1,44 @@
 import os
 import uuid
+from pprint import pprint
 
-from yookassa import Payment, Configuration
+from yookassa import Payment, Configuration, Webhook
+from yookassa.domain.response import PaymentResponse
 from dotenv import load_dotenv
 
 load_dotenv()
 
 Configuration.account_id = int(os.environ.get('YOOKASSA_SHOP_ID'))
 Configuration.secret_key = os.environ.get('YOOKASSA_TOKEN')
+Configuration.configure_auth_token(os.environ.get('YOOKASSA_OAUTH_TOKEN'))
 
-payment = Payment.create({
-    "amount": {
-        "value": "100.00",
-        "currency": "RUB"
-    },
-    "confirmation": {
-        "type": "redirect",
-        "return_url": "https://t.me/CyberNexVpnBot"
-    },
-    "capture": True,
-    "description": "Заказ №1"
-}, uuid.uuid4())
 
-# print(payment.status)
-# print(payment.id)
-# print(payment.json())
+def create_payment(value: int) -> PaymentResponse:
+    payment = Payment.create({
+        "amount": {
+            "value": f"{value}.00",
+            "currency": "RUB"
+        },
+        "confirmation": {
+            "type": "redirect",
+            "return_url": "https://t.me/CyberNexVpnBot"
+        },
+        "capture": True,
+        "description": f"Пополнение баланса на {value} рублей"
+    }, uuid.uuid4())
+    return payment
 
-import requests
 
-url = 'https://yookassa.ru/oauth/v2/authorize?response_type=code&client_id=vf8ebd5fn90l90rcu7nvjrruqqb4omjc&state=test-user'
+# response = Webhook.add({
+#     "event": "payment.succeeded",
+#     "url": "https://www.example.com/notification_url",
+# })
+#
 
-response = requests.get(url)
-print(response.status_code)
-print(response.text)
-print(response.json())
-# LZMmEVtnnv11MSg4h2bzp16L1pbj3z-AeRxR75UxZd6hdsFCcg7thnDq6Ae3x9Zg
+def fill_up_user_balance(user_id: int, value: int):
+    payment = create_payment(value)
+    payment_url = payment['confirmation']['confirmation_url']
+    print(payment.json())
 
+
+fill_up_user_balance(5, 1000)
