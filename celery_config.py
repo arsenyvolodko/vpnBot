14127b11141c3eec71e510d6417944ec17d52340
arsenyvolodko <arvolodko@gmail.com>
2024-06-25 01:57:31 +0300

    remove extra comment

--- celery_config.py
+++ celery_config.py
@@ -1,7 +1,5 @@
 from celery import Celery
 
-# from celery.schedules import crontab
-# from vpnBot import celery_tasks  # do not clear
 
 celery_app = Celery(
     "vpnBot", broker="redis://redis:6379/0", backend="redis://redis:6379/0"
