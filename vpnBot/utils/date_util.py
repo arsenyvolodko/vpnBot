640546523e2259661e1d31bc5a3ea46d40b97655
arsenyvolodko <arvolodko@gmail.com>
2024-06-30 00:35:47 +0300

    fix

--- vpnBot/utils/date_util.py
+++ vpnBot/utils/date_util.py
@@ -6,4 +6,5 @@ from dateutil.relativedelta import relativedelta
 def get_next_date(**kwargs) -> datetime.date:
     start_date = kwargs.get("start_date", datetime.now().date())
     months_delta = kwargs.get("months_delta", 1)
-    return start_date + relativedelta(months=months_delta)
+    hours_delta = kwargs.get("hours_delta", 0)
+    return start_date + relativedelta(months=months_delta, hours=hours_delta)
