d6b8d690eff0d5ac98316773f073b1103cb5bc52
arsenyvolodko <arvolodko@gmail.com>
2025-03-26 18:42:40 +0300

    udpate ip

--- vpnBot/config.py
+++ vpnBot/config.py
@@ -59,8 +59,7 @@ YOOKASSA_OAUTH_TOKEN = os.environ.get("YOOKASSA_OAUTH_TOKEN")
 Configuration.configure_auth_token(YOOKASSA_OAUTH_TOKEN)
 
 ALLOWED_IPS = {
-    "172.18.0.1",
-    "172.23.0.1",
+    "172.25.0.1",
     "127.0.0.1",
     "localhost",
     "192.168.65.1",
